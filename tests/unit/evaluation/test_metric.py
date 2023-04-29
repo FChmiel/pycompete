5b6934450de07f9e0fc81c3a55e0c7c0e9070418
Francis Chmiel <francischmiel@hotmail.co.uk>
2023-04-29 18:49:39 +0100

    Linting

--- tests/unit/evaluation/test_metric.py
+++ tests/unit/evaluation/test_metric.py
@@ -1,6 +1,7 @@
 import pytest
 from pycompete.evaluation.metric import some_metric
 
+
 # unit test for some_metric using pytest parametrize
 @pytest.mark.parametrize("x, y, expected", [(1, 2, 3), (2, 3, 5), (3, 4, 7)])
 def test_some_metric(x, y, expected):
