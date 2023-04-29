05ca182fd845d764e812d11f8935713f7aed0b06
Francis Chmiel <francischmiel@hotmail.co.uk>
2023-04-29 13:36:08 +0100

    Updates to test.yml workflow

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='pycompete',
+      version='0.1',
+      description='Tools to support the training and evalaution of models for the CRUNCHDAO project.',
+      author='Francis P. Chmiel',
+      packages=find_packages(where="pycompete"),  # Required,
+     )
