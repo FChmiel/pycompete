0fc0711976dc5bf11eb64c272499e6dcf5b10ba7
Francis Chmiel <francischmiel@hotmail.co.uk>
2023-05-05 10:51:48 +0100

    Add publish workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="pycompete",
-    version="0.1.0",
     description="Tools to support the training and evalaution of models for the CRUNCHDAO project.",
     author="Francis P. Chmiel",
     packages=find_packages(where="pycompete"),  # Required,
