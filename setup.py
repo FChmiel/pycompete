656084f7c40dea55d6c21be0d0d3e8d5f2920e7e
Francis Chmiel <francischmiel@hotmail.co.uk>
2023-05-05 10:56:34 +0100

    Add setup.cfg

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="pycompete",
-    description="Tools to support the training and evalaution of models for the CRUNCHDAO project.",
-    author="Francis P. Chmiel",
-    packages=find_packages(where="pycompete"),  # Required,
-)
