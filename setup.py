55ad67106b8032ccf9ca90714de1e0611adab6bf
Francis Chmiel <francischmiel@hotmail.co.uk>
2023-05-05 11:26:54 +0100

    linting

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="pycompete",
     description="Tools to support the training and evalaution of models for the CRUNCHDAO project.",
     long_description="Tools to support the training and evalaution of models for the CRUNCHDAO project.",
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author="Francis P. Chmiel",
-    url='https://github.com/FChmiel/pycompete',
-    license='MIT',
+    url="https://github.com/FChmiel/pycompete",
+    license="MIT",
     packages=find_packages(where="pycompete"),  # Required,
 )
